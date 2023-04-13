163b6a2d50e0353b0c66f585542b9d44dafa3afe
Horácio Linhares Monte <98846292+Oraamon@users.noreply.github.com>
2023-04-12 22:53:26 -0300

    first commit

---
+++ app/java/exportkit/figma/landing__activity.java
@@ -0,0 +1,74 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		landing_
+	 *	@date 		Monday 10th of April 2023 08:31:46 PM
+	 *	@title 		Designs
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class landing__activity extends Activity {
+
+	
+	private View _bg__landing__ek2;
+	private TextView sign_in_and_start_your_airmed_experience;
+	private View ellipse_1;
+	private ImageView vector;
+	private View rectangle_5;
+	private View rectangle_7;
+	private View rectangle_8;
+	private ImageView vector_ek1;
+	private ImageView vector_ek2;
+	private TextView usu_rio;
+	private TextView senha;
+	private ImageView icon_social_person_24px;
+	private TextView entrar;
+	private TextView ecocycle;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.landing_);
+
+		
+		_bg__landing__ek2 = (View) findViewById(R.id._bg__landing__ek2);
+		sign_in_and_start_your_airmed_experience = (TextView) findViewById(R.id.sign_in_and_start_your_airmed_experience);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		vector = (ImageView) findViewById(R.id.vector);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		rectangle_7 = (View) findViewById(R.id.rectangle_7);
+		rectangle_8 = (View) findViewById(R.id.rectangle_8);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		usu_rio = (TextView) findViewById(R.id.usu_rio);
+		senha = (TextView) findViewById(R.id.senha);
+		icon_social_person_24px = (ImageView) findViewById(R.id.icon_social_person_24px);
+		entrar = (TextView) findViewById(R.id.entrar);
+		ecocycle = (TextView) findViewById(R.id.ecocycle);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
